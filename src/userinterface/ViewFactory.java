c792933df86196d6346260ad4703854879042a57
Latiq Pratt <magicking577@gmail.com>
2022-03-31 13:33:41 -0400

    This is modify and selection of Books should be working

--- src/userinterface/ViewFactory.java
+++ src/userinterface/ViewFactory.java
@@ -7,78 +7,80 @@ public class ViewFactory {
 
     public static View createView(String viewName, IModel model)
     {
-        if(viewName.equals("LibrarianView") == true)
+        if(viewName.equals("LibrarianView"))
         {
             return new LibrarianView(model);
         }
-        else if(viewName.equals("TransactionChoiceView") == true)
+        else if(viewName.equals("TransactionChoiceView"))
         {
             return new TransactionChoiceView(model);
         }
-        else if(viewName.equals("WorkerView") == true)
+        else if(viewName.equals("WorkerView"))
         {
             return new WorkerView(model);
         }
-        else if(viewName.equals("StudentView") == true)
+        else if(viewName.equals("StudentView"))
         {
             return new AddStudentBorrowerView(model);
         }
-        else if(viewName.equals("BookView") == true)
+        else if(viewName.equals("BookView"))
         {
             return new BookView(model);
         }
-        else if(viewName.equals("SearchWorkerView") == true)
+        else if(viewName.equals("SearchWorkerView"))
         {
             return new SearchWorkersView(model);
         }
-        else if(viewName.equals("WorkerCollectionView") == true)
+        else if(viewName.equals("WorkerCollectionView"))
         {
             return new WorkerCollectionView(model);
         }
 
-        else if(viewName.equals("WithdrawTransactionView") == true)
+        else if(viewName.equals("WithdrawTransactionView"))
         {
             return new WithdrawTransactionView(model);
         }
-        else if(viewName.equals("TransferTransactionView") == true)
+        else if(viewName.equals("TransferTransactionView"))
         {
             return new TransferTransactionView(model);
         }
-        else if(viewName.equals("BalanceInquiryTransactionView") == true)
+        else if(viewName.equals("BalanceInquiryTransactionView"))
         {
             return new BalanceInquiryTransactionView(model);
         }
-        else if(viewName.equals("BalanceInquiryReceipt") == true)
+        else if(viewName.equals("BalanceInquiryReceipt"))
         {
             return new BalanceInquiryReceipt(model);
         }
-        else if(viewName.equals("WithdrawReceipt") == true)
+        else if(viewName.equals("WithdrawReceipt"))
         {
             return new WithdrawReceipt(model);
         }
-        else if(viewName.equals("DepositReceipt") == true)
+        else if(viewName.equals("DepositReceipt"))
         {
             return new DepositReceipt(model);
         }
-        else if(viewName.equals("TransferReceipt") == true)
+        else if(viewName.equals("TransferReceipt"))
         {
             return new TransferReceipt(model);
         }
-        else if(viewName.equals("ModifyWorkerView") == true)
+        else if(viewName.equals("ModifyWorkerView"))
         {
             return new ModifyWorkerView(model);
         }
-        else if(viewName.equals("ModifyBookView") == true)
+        else if(viewName.equals("ModifyBookView"))
         {
             return new ModifyBookView(model);
         }
-        else if(viewName.equals("SearchBooksView") == true)
+        else if(viewName.equals("SearchBooksView"))
         {
             return new SearchBooksView(model);
         }
         else
             return null;
     }
+
+
 
 
 	/*
